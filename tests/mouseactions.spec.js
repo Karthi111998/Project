d0be7ccb7ef360820a7c6692da251a00ef800fc6
Karthi111998 <karthinlpn98@gmail.com>
2026-06-03 09:51:40 +0530

    updated mouse actions

--- tests/mouseactions.spec.js
+++ tests/mouseactions.spec.js
@@ -26,12 +26,6 @@ console.log(text1)
 
 
 
-// await page.locator('#rightBtn').click({button:'right'})
-// const text2=await page.locator('#rightStatus').textContent()
-// console.log(text2)
-
-// await page.getByRole('slider').fill(50)
-// await page.waitForTimeout(4000)
 
 
 })
